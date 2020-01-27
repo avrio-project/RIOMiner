3d278fe708e8d8b5d5661010081a1debec37ca47
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-01-27 05:10:02 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+string daemon_address = '';
+uint32_t port = 0;
+string address = '';
+string miner_identifyer = null;
+string password = 'x';
+cout << "RIOM version " + version;
+cout << "Please enter the pool/daemon address (without port)";
+cin >> daemon_address;
+cout << "Please Enter The Pool/daemon Port";
+cin >> port;
+cout << "Please Enter Your Avrio Address";
+cin >> address;
+cout << "Please enter your miners identifyer (optional)";
+cin >> miner_identifyer;
+
+
